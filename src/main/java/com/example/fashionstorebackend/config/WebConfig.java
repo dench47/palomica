4e1b15e20ecee8107b05d3234942643708c66200
dench47 <dench47@gmail.com>
2025-12-24 17:17:29 +0300

    WebConfig

---
+++ src/main/java/com/example/fashionstorebackend/config/WebConfig.java
@@ -0,0 +1,21 @@
+package com.example.fashionstorebackend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**") // Разрешаем запросы ко всем эндпоинтам, начинающимся с /api
+                .allowedOrigins(
+                        "http://localhost:5173",  // Для локальной разработки
+                        "https://palomica.ru",     // Ваш основной домен (ОБЯЗАТЕЛЬНО с https)
+                        "http://palomica.ru"       // На всякий случай, если кто-то введет без https
+                )
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true); // Если используете куки или авторизацию
+    }
+}
