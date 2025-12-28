0b36f83d4149e7842bacd08c5f3b8a83d5a8f4fd
dench47 <dench47@gmail.com>
2025-12-28 04:23:43 +0300

    Исправление путей к изображениям и баннерам

--- fashion-store-frontend/src/services/api.ts
+++ fashion-store-frontend/src/services/api.ts
@@ -13,13 +13,13 @@ export type Product = {
     category: string;
 }
 
+//
+// const API_BASE_URL = import.meta.env.DEV
+//     ? 'http://localhost:8085/api'
+//     : 'https://api.palomika.ru/api';
 
-const API_BASE_URL = import.meta.env.DEV
-    ? 'http://localhost:8085/api'
-    : 'https://api.palomika.ru/api';
 
-
-// const API_BASE_URL = 'https://api.palomika.ru/api';
+const API_BASE_URL = 'https://api.palomika.ru/api';
 
 
 export const productService = {
